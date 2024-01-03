3a5eb1a761dfe36bd26e9e469954e4c114579d86
Farhan <146110996+HanNewbie@users.noreply.github.com>
2024-01-03 13:33:16 +0700

    tubes

--- TUBES_DPW/routes/web.php
+++ TUBES_DPW/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\Admin\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/admin')->middleware('auth')->group(function() { // Mengatur rute untuk admin dengan middleware auth
+    Route::get('/', function () { // Mengatur rute untuk halaman dashboard admin
+        return view('admin.dashboard'); // Menampilkan view admin.dashboard
+    });
+    Route::resource('cars', CarController::class);
+    Route::resource('cars', ProductController::class);
 });
+
+
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
+Route::get('contact', [\App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
+Route::get('detail', [\App\Http\Controllers\HomeController::class, 'detail'])->name('detail');
+
+Route::get('admin/dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard.index')->middleware('is_admin');
+Route::resource('admin/cars', \App\Http\Controllers\Admin\CarController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
